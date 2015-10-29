b2eaabdda127f14ed276b1ad551db485802e9ea8
lukeschunk <lukeschunk@gmail.com>
2015-10-28 18:21:07 -0600

    finishe d crux of assignment

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,17 +6,35 @@ app.controller('mainCtrl', function ($scope, parseService) {
     //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
     //your controllers $scope as messages ($scope.messages)
 
+    
+    $scope.getParseData = function() {
+        parseService.getData()
+            .then(function (updatedResult) {
+//                console.log("this is from the controller", updatedResult);
+//                var whatever = updatedResult[3].text;
+//                console.log("this is whatever", whatever);
+                $scope.messages = updatedResult;
+            });
+    };
+    
 
-
+    $scope.getParseData();
     //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
     //pass that text to the postData method on the parseService object which will then post it to the parse backend.
 
+    $scope.postData = function () {
+        parseService.postData($scope.message)
+            .then(function (response) {
+            console.log("this is response", response)
+            
+            $scope.getParseData();
+        });
+    }
 
 
     //uncomment this code when your getParseData function is finished
     //This goes and gets new data every second, which mimicking a chat room experience.
-    
-//    setInterval(function () {
-//        $scope.getParseData();
-//    }, 1500)
+    setInterval(function () {
+        $scope.getParseData();
+    }, 120039943)
 })
